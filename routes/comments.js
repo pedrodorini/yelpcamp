925e402e3ba0c132de046239f0f12178792d640f
Pedro Henrique <pedro.dorini@hotmail.com>
2018-01-16 17:30:37 -0200

    add delete and edit comment

--- routes/comments.js
+++ routes/comments.js
@@ -36,6 +36,39 @@ router.post('/', isLoggedIn, (req, res) => {
 	})
 })
 
+router.get('/:comment_id/edit', (req, res) => {
+	Comment.findById(req.params.comment_id, (err, comment) => {
+		if (err) {
+			console.log(err)
+			res.redirect('back')
+		} else {
+			res.render('comments/edit', {campground_id: req.params.id, comment: comment})
+		}
+	})
+})
+
+router.put('/:comment_id', (req, res) => {
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, comment) => {
+		if (err) {
+			console.log(err)
+			res.redirect('back')
+		} else {
+			res.redirect(`/campgrounds/${req.params.id}`)
+		}
+	})
+})
+
+router.delete('/:comment_id', (req, res) => {
+	Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+		if (err) {
+			console.log(err)
+			res.redirect('back')
+		} else {
+			res.redirect(`/campgrounds/${req.params.id}`)
+		}
+	})
+})
+
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated()) {
 		return next()
